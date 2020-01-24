65c2e8d5c91f22c9e56ab59c5db1ea7fabf6df91
Yusuke Andoh <yusukeandoh@gmail.com>
2020-01-24 17:05:03 +0900

    Updates

---
+++ precache-manifest.a418339176597fa7a6336fe242f10c32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2490ed80889a1340013469057763a78",
+    "url": "/lcard/index.html"
+  },
+  {
+    "revision": "4c92579e2353eac49e8b",
+    "url": "/lcard/static/css/main.e73f111c.chunk.css"
+  },
+  {
+    "revision": "c06ca60174c00c7aac27",
+    "url": "/lcard/static/js/2.886aad27.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/lcard/static/js/2.886aad27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c92579e2353eac49e8b",
+    "url": "/lcard/static/js/main.9a9565ec.chunk.js"
+  },
+  {
+    "revision": "23695f931469473a5403",
+    "url": "/lcard/static/js/runtime-main.bdc23d3e.js"
+  }
+]);
